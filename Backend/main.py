1fa36dcc75f278eb62efd1cd9bcc52d5c4b0f099
sirajulislam16 <sirajulislam160808@gmail.com>
2026-03-08 23:01:38 +0600

    added updated API

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models import CodeRequest
 from ai_engine import review_code
 
 app = FastAPI()
 
-class CodeRequest(BaseModel):
-    code: str
-
 @app.post("/review")
+
 async def review(request: CodeRequest):
 
     result = review_code(request.code)
